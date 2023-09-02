c99e11b61365ba8453752c80d9ed12d33ecee9a6
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-03 02:55:37 +0700

    Add title on LoginPage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react';
+import { useDocumentTitle } from 'usehooks-ts';
 import logo from '../../assets/logo-big.png';
 import LoginForm from '../../features/login-form/LoginForm';
 import styles from './LoginPage.module.scss';
 
 
-const LoginPage: FC = () => (
-    <div className={styles.root}>
-        <img src={logo} alt='Translate Subs Player' />
-        <LoginForm />
-    </div>
-);
+const LoginPage: FC = () => {
+    useDocumentTitle('Login | Translate Subs Player');
+
+    return (
+        <div className={styles.root}>
+            <img src={logo} alt='Translate Subs Player' />
+            <LoginForm />
+        </div>
+    );
+};
 
 export default LoginPage;
