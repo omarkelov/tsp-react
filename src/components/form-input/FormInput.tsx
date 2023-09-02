82b958258bcdc823fb5de6bb375da3399442ffb8
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-03 02:20:05 +0700

    Create FormInput

---
+++ src/components/form-input/FormInput.tsx
@@ -0,0 +1,26 @@
+import { FC, RefAttributes, forwardRef } from 'react';
+import { combineClassNames } from '../../util/classNames';
+import Input from '../input/Input';
+import styles from './FormInput.module.scss';
+
+
+const FormInput: FC<{
+    label: string;
+    type?: React.HTMLInputTypeAttribute;
+    className?: string;
+} & RefAttributes<HTMLInputElement>> = forwardRef(({ label, type, className }, ref) => (
+    <section className={styles.root}>
+        <label>
+            {label}:
+        </label>
+        <br />
+        <Input
+            ref={ref}
+            label={label}
+            type={type}
+            className={combineClassNames(styles.eInput, className)}
+        />
+    </section>
+));
+
+export default FormInput;
