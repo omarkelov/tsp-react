ae107bbe484063d0b8da1c0a302932a862530ee6
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-03 02:19:46 +0700

    Create Input

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,19 @@
+import { FC, RefAttributes, forwardRef } from 'react';
+import { combineClassNames } from '../../util/classNames';
+import styles from './Input.module.scss';
+
+
+const Input: FC<{
+    label: string;
+    type?: React.HTMLInputTypeAttribute;
+    className?: string;
+} & RefAttributes<HTMLInputElement>> = forwardRef(({ label, type, className }, ref) => (
+    <input
+        ref={ref}
+        className={combineClassNames(styles.input, className)}
+        type={type}
+        placeholder={label}
+    />
+));
+
+export default Input;
