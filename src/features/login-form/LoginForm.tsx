1f953148f8859050e36502a987efddff04a8d9bf
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-03 02:22:22 +0700

    Create LoginForm

---
+++ src/features/login-form/LoginForm.tsx
@@ -0,0 +1,42 @@
+import { FC, useRef } from 'react';
+import Button from '../../components/button/Button';
+import Form from '../../components/Form';
+import FormInput from '../../components/form-input/FormInput';
+import { loginAsync } from '../../store/authSlice';
+import { useAppDispatch } from '../../store/hooks';
+import styles from './LoginForm.module.scss';
+
+
+const LoginForm: FC = () => {
+    const loginRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const dispatch = useAppDispatch();
+
+    const handleLoginClick = () => {
+        if (loginRef.current?.value.length && passwordRef.current?.value.length) {
+            dispatch(loginAsync({
+                login: loginRef.current.value,
+                password: passwordRef.current.value,
+            }));
+        }
+    };
+
+    return (
+        <Form className={styles.root}>
+            <FormInput
+                ref={loginRef}
+                label='Login'
+                type='text' />
+            <FormInput
+                ref={passwordRef}
+                label='Password'
+                type='password' />
+            <Button
+                value='Login'
+                onClick={handleLoginClick} />
+        </Form>
+    );
+};
+
+export default LoginForm;
