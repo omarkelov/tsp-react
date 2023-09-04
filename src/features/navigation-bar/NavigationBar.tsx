c5a355f832a3d86d9cea3771e082c76c9e11032e
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-05 01:18:59 +0700

    Create NavigationBar

--- src/features/navigation-bar/NavigationBar.tsx
+++ src/features/navigation-bar/NavigationBar.tsx
@@ -1,16 +1,32 @@
 import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { logoutAsync } from '../../store/authSlice';
 import { useAppDispatch } from '../../store/hooks';
+import { combineClassNames } from '../../util/classNames';
+
+import styles from './NavigationBar.module.scss';
+
 
 
 const NavigationBar: FC = () => {
     const dispatch = useAppDispatch();
 
+    const onLogoutClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        dispatch(logoutAsync());
+    };
+
+    const generateClassNames = ({ isActive }: { isActive: boolean }) => isActive
+        ? combineClassNames(styles.link, styles.mLinkActive)
+        : styles.link;
+
     return (
-        <div>
-            <button onClick={() => dispatch(logoutAsync())}>Logout</button>
-        </div>
+        <nav className={styles.root}>
+            <NavLink className={generateClassNames} to='/movies'>Movies</NavLink>
+            <NavLink className={generateClassNames} to='/dictionaries'>Dictionaries</NavLink>
+            <a className={styles.link} href='/logout' onClick={onLogoutClick}>Logout (TODO)</a>
+        </nav>
     );
 };
 
