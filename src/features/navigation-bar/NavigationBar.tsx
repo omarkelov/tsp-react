ec080446375a8360ac0dba4b5aadf1a68dd494bc
Oleg Markelov <o.markelov@g.nsu.ru>
2023-09-28 00:10:08 +0700

    Switch to simple Link in NavigationBar

--- src/features/navigation-bar/NavigationBar.tsx
+++ src/features/navigation-bar/NavigationBar.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { logoutAsync } from '../../store/authSlice';
 import { useAppDispatch } from '../../store/hooks';
-import { combineClassNames } from '../../util/classNames';
 
 import styles from './NavigationBar.module.scss';
 
@@ -17,14 +16,10 @@ const NavigationBar: FC = () => {
         dispatch(logoutAsync());
     };
 
-    const generateClassNames = ({ isActive }: { isActive: boolean }) => isActive
-        ? combineClassNames(styles.link, styles.mLinkActive)
-        : styles.link;
-
     return (
         <nav className={styles.root}>
-            <NavLink className={generateClassNames} to='/movies'>Movies</NavLink>
-            <NavLink className={generateClassNames} to='/dictionaries'>Dictionaries</NavLink>
+            <Link className={styles.link} to='/movies'>Movies</Link>
+            <Link className={styles.link} to='/dictionaries'>Dictionaries</Link>
             <a className={styles.link} href='/logout' onClick={onLogoutClick}>Logout (TODO)</a>
         </nav>
     );
