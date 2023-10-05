baf7193ced2be598b7ce5c8565fc2706871df5f4
Oleg Markelov <o.markelov@g.nsu.ru>
2023-10-06 02:37:42 +0700

    Update endpoints

--- src/api/dictionariesAPI.ts
+++ src/api/dictionariesAPI.ts
@@ -4,12 +4,12 @@ export interface Dictionary {
 }
 
 export const fetchDictionaries = (page: number, limit: number) =>
-    fetch(`http://localhost:8081/movies?page=${page}&limit=${limit}`, {
+    fetch(`http://localhost:8081/api/movies?page=${page}&limit=${limit}`, {
         credentials: 'include',
         method: 'GET',
     });
 
-export const fetchDeleteDictionary = (dictionaryName: string) => fetch(`http://localhost:8081/movies/${dictionaryName}`, {
+export const fetchDeleteDictionary = (dictionaryName: string) => fetch(`http://localhost:8081/api/movies/${dictionaryName}`, {
     credentials: 'include',
     method: 'DELETE',
 });
