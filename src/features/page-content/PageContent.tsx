3958af8da037c85aa79861e17cc45d41a53443de
Oleg Markelov <o.markelov@g.nsu.ru>
2023-10-09 18:28:09 +0700

    Simplify PageContent

--- src/features/page-content/PageContent.tsx
+++ src/features/page-content/PageContent.tsx
@@ -3,17 +3,14 @@ import { FC, ReactNode } from 'react';
 import styles from './PageContent.module.scss';
 
 
-
 const PageContent: FC<{
     title?: string;
     children?: ReactNode;
-}> = ({ title, children }) => {
-    return (
-        <div>
-            {title && <h1 className={styles.title}>{title}</h1>}
-            {children}
-        </div>
-    );
-};
+}> = ({ title, children }) => (
+    <div>
+        {title && <h1 className={styles.title}>{title}</h1>}
+        {children}
+    </div>
+);
 
 export default PageContent;
