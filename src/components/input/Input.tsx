1ba0569f23ee728181a8c4aaf314dc65cb75036d
Oleg Markelov <o.markelov@g.nsu.ru>
2023-10-24 23:06:56 +0700

    Add isDisabled for input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -8,13 +8,15 @@ import styles from './Input.module.scss';
 const Input: FC<{
     label: string;
     type?: React.HTMLInputTypeAttribute;
+    isDisabled?: boolean;
     className?: string;
-} & RefAttributes<HTMLInputElement>> = forwardRef(({ label, type, className }, ref) => (
+} & RefAttributes<HTMLInputElement>> = forwardRef(({ label, type, isDisabled, className }, ref) => (
     <input
         ref={ref}
         className={combineClassNames(styles.input, className)}
         type={type}
         placeholder={label}
+        disabled={isDisabled}
     />
 ));
 
